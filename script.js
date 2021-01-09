9d139b5930900bfac366a9a5e6b35afb4003e837
Fibre P <fttx5525@gmail.com>
2021-01-09 20:22:05 +1100

    Refactoring

--- script.js
+++ script.js
@@ -1,16 +1,17 @@
-// Squares //
+// Default Squares per side //
 let squares = 14;
+// Array to store squareDivs //
 let squareDivs = [];
-// Button //
+// The Grid //
+let sketchGrid = document.querySelector(".sketch-grid");
+// Buttons //
 let clearBtn = document.querySelector(".clear");
-let chunkyBtn = document.querySelector("#chunky");
-let regularBtn = document.querySelector("#regular");
-let fineBtn = document.querySelector("#fine");
+let gridBtns = document.querySelectorAll(".grid-btn")
 let noirBtn = document.querySelector("#noir");
 let blancBtn = document.querySelector("#blanc");
 let varBtn = document.querySelector("#variation");
-let sketchGrid = document.querySelector(".sketch-grid");
 let checkbox = document.querySelector("#checkbox");
+// Drawing Subjects //
 let templates = document.querySelector(".templates");
 templates.style.display = "none";
 // Deafult Grid //
@@ -20,26 +21,16 @@ sketchGrid.addEventListener("mouseover", fillNoir);
 // add onclick function to clear button to call clearGrid function
 clearBtn.addEventListener("click", clearGrid);
 // add onclick function to grid size buttons to call changeGridSize function
-chunkyBtn.addEventListener("click", changeGridSize);
-regularBtn.addEventListener("click", changeGridSize);
-fineBtn.addEventListener("click", changeGridSize);
+for (i=0;i<gridBtns.length;i++) {
+    gridBtns[i].addEventListener("click", changeGridSize);
+}
 // add onclick function to grid color buttons to call changeEvent listener
 noirBtn.addEventListener("click", addNoirListener);
 blancBtn.addEventListener("click", addBlancListener);
 varBtn.addEventListener("click", addVarListener);
 // add onclick function to checkbox and call toggleTemplates
 checkbox.addEventListener("click", toggleTemplates);
-// function to hide/show template images
-function toggleTemplates() {
-    switch (templates.style.display) {
-        case ("none"):
-            templates.style.display = "block";
-            break;
-        case ("block"):
-            templates.style.display = "none";
-            break;
-    }
-}
+
 // function to add event listener to sketchGrid - Noir
 function addNoirListener() {
     sketchGrid.removeEventListener("mouseover", fillBlanc);
@@ -58,24 +49,38 @@ function addVarListener() {
     sketchGrid.removeEventListener("mouseover", fillBlanc);
     sketchGrid.addEventListener("mouseover", fillVar); 
 }
-// Create 16x16 grid of square divs USING JS inside .sketch-grid //
+// create function to switch between grid sizes depending on which button was clicked
+function changeGridSize(e) {
+    switch (e.target.id) {
+        case "chunky":
+            squares = 14;
+            break;
+        case "regular":
+            squares = 50;
+            break;
+        case "fine":
+            squares = 100;
+            break;
+    }
+    sketchGrid.textContent = "";
+    createGrid(squares);
+}
+// create function that removes .fill from each squareDiv in squareDivs array
+function clearGrid() {
+    for (i=0;i<(squares**2);i++) {
+        squareDivs[i].style.backgroundColor = "transparent";
+    }
+}
+// Create squares x squares grid of squareDivs inside .sketch-grid //
 function createGrid(squares) {
     sketchGrid.style.gridTemplateColumns = `repeat(${squares}, 1fr)`;
     sketchGrid.style.gridTemplateRows = `repeat(${squares}, 1fr)`;
     // create square divs and append to sketchGrid
-    // for each column
-    let i = 0; // index of squareDivs
-    for (x=1;x<=squares;x++) {
-        // for each row
-        for (y=1;y<=squares;y++) {
-            squareDivs[i] = document.createElement('div');
-            squareDivs[i].style.gridColumn = `${x} / (${x}+1)`;
-            squareDivs[i].style.gridRow = `${y} / (${y} + 1)`;
+    for (i=0;i<squares**2;i++) {
+        squareDivs[i] = document.createElement('div');
             squareDivs[i].style.border = `1px solid black`;
             squareDivs[i].style.borderRadius= '3%';
             sketchGrid.appendChild(squareDivs[i]);
-            i++;
-        }
     }
 }
 // create function that changes background color of div as mouse enters element to BLACK
@@ -99,26 +104,15 @@ function fillVar(e) {
         e.target.style.backgroundColor = colors[Math.floor(Math.random()*colors.length)];
     }
 }
-// create function that removes .fill from each squareDiv in squareDivs array
-function clearGrid() {
-    for (i=0;i<(squares**2);i++) {
-        squareDivs[i].style.backgroundColor = "transparent";
-    }
-}
-// create function to switch between grid sizes depending on which button was clicked
-function changeGridSize(e) {
-    switch (e.target.id) {
-        case "chunky":
-            squares = 14;
+// function to hide/show template images
+function toggleTemplates() {
+    switch (templates.style.display) {
+        case ("none"):
+            templates.style.display = "block";
             break;
-        case "regular":
-            squares = 50;
-            break;
-        case "fine":
-            squares = 100;
+        case ("block"):
+            templates.style.display = "none";
             break;
     }
-    sketchGrid.textContent = "";
-    createGrid(squares);
 }
-// keep the total .sketch-grid to be the same i.e. make the size of squares smaller if number of squares increase, make the squares bigger if less number of squares per side
+
